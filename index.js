ba30a9248c6f696aa282486b575e183388c77db8
chandannath98 <84062359+chandannath98@users.noreply.github.com>
2023-08-09 15:12:38 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Handle socket connections
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  // Handle incoming messages
+  socket.on('message', (message) => {
+    console.log('Received message:', message);
+    // Broadcast the message to all connected clients
+    io.emit('message', message);
+  });
+
+  // Handle disconnections
+  socket.on('disconnect', () => {
+    console.log('A user disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
