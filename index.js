5f9fc3c4b2e8578e23049ec2c2b7820ff1434225
chandannath98 <84062359+chandannath98@users.noreply.github.com>
2023-08-26 22:33:50 +0530

    Update index.js - user online status feature added

--- index.js
+++ index.js
@@ -6,12 +6,27 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+
+var usersConnected =[]
 // Handle socket connections
 io.on('connection', (socket) => {
   // console.log('A user connected:', socket.id);
       io.to(socket.id).emit('SocketID', socket.id);
-
+      usersConnected.push(socket.id)
   // Handle incoming messages
+
+ socket.on('checkUserStatus', (id) => {
+
+   const isOnline = usersConnected.find((i)=>i ==id) ?
+   
+   
+      io.to(socket.id).emit('userStatus', isOnline);
+    
+  });
+
+  
+
+  
   socket.on('message', (message) => {
     console.log('Received message:', message);
     // Broadcast the message to all connected clients
@@ -29,6 +44,8 @@ io.on('connection', (socket) => {
 
   // Handle disconnections
   socket.on('disconnect', () => {
+     usersConnected=usersConnected.filter((i)=>i != socket.id)
+    
     console.log('A user disconnected:', socket.id);
      io.emit('disconnected_user', socket.id);
   });
